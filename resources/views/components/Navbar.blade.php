44117a616f30f49e4d4413d702b5b73e47b5d6a8
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-03-15 10:43:22 +0200

    fix: the toggle button of Hamburger menu

--- resources/views/components/Navbar.blade.php
+++ resources/views/components/Navbar.blade.php
@@ -1,17 +1,15 @@
 <!-- Navbar section -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <section class="container">
-
+    <div class="container-fluid">
         <a class="navbar-brand" href="{{url("/")}}">My Blog</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{url("/")}}">Home</a>
+                    <a class="nav-link active" href="{{url("/")}}">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{url("posts/trash")}}">Trash</a>
@@ -21,5 +19,5 @@
                 </li>
             </ul>
         </div>
-    </section>
+    </div>
 </nav>
