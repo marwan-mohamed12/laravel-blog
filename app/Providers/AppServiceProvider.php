0dacd78223eb4f7cc08eecfd4261c497327ad1de
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-03-18 00:00:28 +0200

    fix: Force Laravel to accept only https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('APP_ENV') == 'production') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
