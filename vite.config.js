e96cf711a480d088162257899d4da09205e9f140
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-03-17 16:45:26 +0200

    fix:added fix to tailwind config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: 'localhost',
+    },
     plugins: [
         laravel({
             input: [
