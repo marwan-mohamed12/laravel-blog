08d5606a6a516c8b1793186f57ed4296c497e47e
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-03-17 15:06:56 +0200

    fix: the home link

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- Navbar section -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{url("/")}}">My Blog</a>
+        <a class="navbar-brand" href="{{url("//posts")}}">My Blog</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -9,13 +9,16 @@
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" href="{{url("/")}}">Home</a>
+                    <a class="nav-link active" href="{{url("/posts")}}">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{url("posts/trash")}}">Trash</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{url("posts/table")}}">Posts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{url("posts/create")}}">Add Post</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{url("users")}}">Users</a>
