4c80006d350fbfea205d2dd618cb1d4925ee1ee0
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-03-17 16:14:22 +0200

    chore:changed root route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/register');
 });
 
 Route::get('/dashboard', function () {
